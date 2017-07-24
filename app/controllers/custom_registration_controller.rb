408fa7f9bb793d1a5421326a299a2872e8f14c85
Ben <7rubengarcia7@gmail.com>
2017-07-24 08:15:19 -0700

    Commented out code that sends email after creating an account

--- app/controllers/custom_registration_controller.rb
+++ app/controllers/custom_registration_controller.rb
@@ -2,7 +2,7 @@ class CustomRegistrationController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      UsersMailer.welcome_email(@user).deliver_now
+      # UsersMailer.welcome_email(@user).deliver_now
     end
   end
 end
